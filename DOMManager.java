935a6630654c34f72f18fe6e17d2796cc9be0717
AdrianGarciaB <33894550+AdrianGarciaB@users.noreply.github.com>
2019-12-04 23:01:39 +0100

    Create DOMManager.java

---
+++ DOMManager.java
@@ -0,0 +1,82 @@
+package com.examen;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+
+public class DOMManager {
+    private Document doc;
+    private File file;
+    public enum MODE{NEW, READ, ADD, REMOVE};
+    private Element actualElement;
+    private MODE mode;
+
+    public DOMManager(MODE mode, File file, String rootElement) throws ParserConfigurationException {
+        this.file = file;
+        this.mode = mode;
+        if (mode == MODE.NEW) {
+            doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+            actualElement = doc.createElement(rootElement);
+            doc.appendChild(actualElement);
+        }
+
+    }
+
+    public void setActualElementToParent(){
+        actualElement = (Element) actualElement.getParentNode();
+    }
+
+
+
+    public void write(String key, String value){
+        Element child = doc.createElement(key);
+        child.appendChild(doc.createTextNode(value));
+        actualElement.appendChild(child);
+    }
+
+    public void write(String key, boolean writeInto){
+        Element child = doc.createElement(key);
+        actualElement.appendChild(child);
+        if (writeInto) actualElement = child;;
+    }
+
+    public void writeAttribute(String key, String value){
+        Element child = doc.createElement(key);
+        child.appendChild(doc.createTextNode(value));
+        actualElement.appendChild(child);
+    }
+
+    public void writeAttribute(String key, boolean writeInto){
+        Element child = doc.createElement(key);
+        actualElement.appendChild(child);
+        if (writeInto) actualElement = child;;
+    }
+
+    public void read(){
+
+    }
+
+    public void add(){}
+
+    public void close() throws TransformerException {
+        if (mode == MODE.NEW){
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(file);
+            transformer.transform(source, result);
+        }
+    }
+
+    public void remove(){
+
+    };
+}
